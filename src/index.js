cf215c648b087fc948a5c49b64ee23e250d8b54f
danvusca <dan.vusca@cegeka.com>
2018-02-14 18:19:25 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import 'core-js'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+
+import 'semantic-ui-css/semantic.min.css'
+
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+          Working
+        </div>
+    </BrowserRouter>, document.getElementById('root')
+)
